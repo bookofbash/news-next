c1644289851d28aa6d1c1f715f79b3d70eae7701
Bashir Harrell <bashir@Bashirs-MacBook-Air.local>
2022-11-07 16:31:55 -0500

    Second commit

---
+++ pages/dom.js
@@ -0,0 +1,22 @@
+const DOM = ({ developer }) => {
+    console.log(developer)
+return (
+        <div className='page-container'>
+            <div>
+                <h1>Developer of the month</h1>
+            </div>
+        </div>
+    )
+}
+export const getServerSideProps = async () => {
+    const apiResponse = await fetch(
+        'https://my-json-server.typicode.com/nabendu82/news-next/devOfMonth',
+    );
+    const developer = await apiResponse.json();
+return {
+        props: {
+            developer,
+        },
+    };
+};
+export default DOM
